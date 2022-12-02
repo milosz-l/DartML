665b663062a664b878df66fc1a0371064d48a05f
Milosz Lopatto <milosz.lopatto@gmail.com>
2022-12-02 16:32:45 +0100

    working heatmap and pairplot

--- pages/1_Sample.py
+++ pages/1_Sample.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # Upload Data
 st.title("Upload Data")
@@ -11,7 +14,18 @@ if uploaded_file is not None:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     string_data = stringio.read()
     dataframe = pd.read_csv(uploaded_file)
+    dataframe = dataframe.iloc[:, -10:]  # TODO: at the moment it keeps only last 10 columns - delete this
     st.session_state['df'] = dataframe
+
+    # TODO: comment/uncomment below and check whether it's faster
+    # calculate heatmap
+    st.session_state['heatmap_fig'] = plt.figure(figsize=(16, 16))
+    sns.heatmap(st.session_state['df'].corr(numeric_only=True), square=True, cmap='RdYlGn', annot=True, fmt=".2f", linewidth=0.5)
+
+    # calculate pairplot
+    # st.session_state['pairplot_fig'] = plt.figure(figsize=(16, 16))
+    st.session_state['pairplot_fig'] = sns.pairplot(st.session_state['df'])
+
 
 # show uploaded df
 if 'df' in st.session_state:
--- pages/2_Explore.py
+++ pages/2_Explore.py
@@ -1,11 +1,30 @@
 import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Transform Data
 st.title("Explore Data")
 
-# show uploaded df
 if 'df' in st.session_state:
+    # show uploaded df
     st.write('Uploaded DataFrame:')
     st.write(st.session_state['df'])
 
-# Drop columns
+    # calculate correlation heatmap if not done yet
+    # TODO: select features to show on heatmap
+    if 'heatmap_fig' not in st.session_state:
+        st.session_state['heatmap_fig'] = plt.figure(figsize=(16, 16))
+        sns.heatmap(st.session_state['df'].corr(numeric_only=True), square=True, cmap='RdYlGn', annot=True, fmt=".2f", linewidth=0.5)
+
+    # calculate pairplot if not done yet
+    # TODO: select kind of pairplot
+    # TODO: select optional hue of pairplot
+    if 'pairplot_fig' not in st.session_state:
+        # st.session_state['pairplot_fig'] = plt.figure(figsize=(16, 16))
+        st.session_state['pairplot_fig'] = sns.pairplot(st.session_state['df'])
+
+    # show visualizations
+    st.write('Correlation heatmap:')
+    st.pyplot(st.session_state['heatmap_fig'])
+    st.write('Pairplot:')
+    st.pyplot(st.session_state['pairplot_fig'])
