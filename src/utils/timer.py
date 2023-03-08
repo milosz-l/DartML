188fa281ddcacdc6974967b6baaf3e7136915dd7
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-03-08 22:55:16 +0100

    added @wraps decorator

--- src/utils/timer.py
+++ src/utils/timer.py
@@ -1,11 +1,15 @@
+from functools import wraps
 import time
 
 
 def add_timer_note(func):
+
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
         time_note = f"{func.__name__} took {round(end - start, 4)}s to run"
         return result, time_note
+
     return wrapper
