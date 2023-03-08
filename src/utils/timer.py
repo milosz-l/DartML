b3f45e256ab5b188c9b076d82d53bec73a0c1b9c
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-03-08 22:30:06 +0100

    added timer decorator

---
+++ src/utils/timer.py
@@ -0,0 +1,11 @@
+import time
+
+
+def add_timer_note(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        time_note = f"{func.__name__} took {round(end - start, 4)}s to run"
+        return result, time_note
+    return wrapper
