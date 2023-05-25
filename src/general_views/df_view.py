e6db34ecf2b902fbf79c543d5bf85a0cc14067d3
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-05-25 18:05:26 +0200

    added additional check in df_view

--- src/general_views/df_view.py
+++ src/general_views/df_view.py
@@ -1,11 +1,14 @@
 import streamlit as st
+from src.session_state.session_state_checks import df_in_session_state, sampled_df_in_session_state
 
 
 def show_uploaded_df(expanded=False):
-    with st.expander("Uploaded data", expanded=expanded):
-        st.write(st.session_state.df)
+    if df_in_session_state():
+        with st.expander("Uploaded data", expanded=expanded):
+            st.write(st.session_state.df)
 
 
 def show_sampled_df(expanded=False):
-    with st.expander("Sampled data", expanded=expanded):
-        st.write(st.session_state.sampled_df)
+    if sampled_df_in_session_state():
+        with st.expander("Sampled data", expanded=expanded):
+            st.write(st.session_state.sampled_df)
