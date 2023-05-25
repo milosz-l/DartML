6ec1ea010d9d4473bc0c021b152267081d606209
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-05-25 17:58:25 +0200

    made explore section use sampled data

--- pages/2_🔍_Explore.py
+++ pages/2_🔍_Explore.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from src.explore.PlotBuilder import PlotBuilder
-from src.general_views.df_view import show_uploaded_df
+from src.general_views.df_view import show_sampled_df
 from src.general_views.sidebars_view import show_info_sidebar
 from src.session_state.session_state_checks import df_in_session_state
 from src.explore.plots_view import show_heatmap, show_pairplot
@@ -13,15 +13,17 @@ st.title("🔍 Explore")
 if not df_in_session_state():
     st.write("You need to upload some data first! Please go to Sample tab.")
 else:
-    show_uploaded_df(expanded=False)
+    show_sampled_df(expanded=False)
     show_info_sidebar()
 
     st.divider()
 
     with st.expander("Show correlation heatmap", expanded=False):
         show_regenerate_heatmap_button()
-        show_heatmap()
+        if not st.session_state.sampled_df.empty:
+            show_heatmap()
 
     with st.expander("Show pairplot", expanded=False):
         show_regenerate_pairplot_button()
-        show_pairplot()
+        if not st.session_state.sampled_df.empty:
+            show_pairplot()
--- src/explore/plots_view.py
+++ src/explore/plots_view.py
@@ -7,7 +7,7 @@ def show_heatmap():
     start_time = time.time()
     if "heatmap" not in st.session_state:
         with st.spinner("Generating heatmap"):
-            st.session_state.heatmap = PlotBuilder(st.session_state.df).get_corr_heatmap()
+            st.session_state.heatmap = PlotBuilder(st.session_state.sampled_df).get_corr_heatmap()
     st.image(st.session_state.heatmap, channels="RGB")
     end_time = time.time()
     st.write(f"Showing the above plot took {end_time - start_time:.2f}s")
@@ -17,7 +17,7 @@ def show_pairplot():
     start_time = time.time()
     if "pairplot" not in st.session_state:
         with st.spinner("Generating pairplot"):
-            st.session_state.pairplot = PlotBuilder(st.session_state.df).get_pairplot()
+            st.session_state.pairplot = PlotBuilder(st.session_state.sampled_df).get_pairplot()
     st.image(st.session_state.pairplot, channels="RGB")
     end_time = time.time()
     st.write(f"Showing the above plot took {end_time - start_time:.2f}s")
