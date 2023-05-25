55a1b762466804e6f0b23fee8f2ade0ca53d39fb
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-05-25 10:33:59 +0200

    finished initial testing of mljar

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # thesis
 Engineering thesis app in streamlit
+
+
+# Installation
+
+## Using conda
+You can change `new_env_name` to any name you like.
+
+```bash
+conda create --name new_env_name python=3.9
+pip install -r requirements.txt
+```
+
---
+++ mljar_test2_multiclass_classification.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# scikit learn utilites
+from sklearn.datasets import load_digits
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+# mljar-supervised package
+from supervised.automl import AutoML
+
+# load the data
+digits = load_digits()
+X_train, X_test, y_train, y_test = train_test_split(pd.DataFrame(digits.data), digits.target, stratify=digits.target, test_size=0.25, random_state=123)
+
+# train models with AutoML
+automl = AutoML(mode="Perform")
+automl.fit(X_train, y_train)
+
+# compute the accuracy on test data
+predictions = automl.predict_all(X_test)
+print(predictions.head())
+print("Test accuracy:", accuracy_score(y_test, predictions["label"].astype(int)))
