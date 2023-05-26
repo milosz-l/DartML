880099636905d14f0ee60dadcac8046d6403f889
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-05-26 19:25:52 +0200

    added sidebar in Model section

--- pages/4_🤖_Model.py
+++ pages/4_🤖_Model.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from src.session_state.session_state_checks import sampled_df_in_session_state
 from src.general_views.mljar_explain_view import show_mljar_explain
+from src.general_views.sidebars_view import show_info_sidebar
+
 
 # Train Model
 st.title("🤖 Model")
@@ -8,6 +10,7 @@ st.title("🤖 Model")
 if not sampled_df_in_session_state():
     st.write("You need to upload some data first! Please go to Sample tab.")
 else:
+    show_info_sidebar()
     show_mljar_explain()
 
     # st.write("## Scores")  # TODO: change to automatic
