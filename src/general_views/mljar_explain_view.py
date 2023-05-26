974521487dc40054ea95f35593b7df1091ec6cb0
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-05-26 18:13:21 +0200

    added current date and time to the name of report zipfile

--- src/general_views/mljar_explain_view.py
+++ src/general_views/mljar_explain_view.py
@@ -8,6 +8,7 @@ import io
 import tempfile
 import os
 import zipfile
+from datetime import datetime
 
 
 class OutputRedirector:  # TODO: remove it in prod and lower the verbosity level of AutoML
@@ -47,32 +48,34 @@ def show_mljar_explain():
         with st.form("EDA_form"):
             target_col_name = simple_target_column_selectbox()
             submitted = st.form_submit_button("Generate report")
-            if submitted:
-                with st.spinner("Generating report..."):
-                    with tempfile.TemporaryDirectory() as tmpdirname:
-                        X_train, X_test, y_train, y_test = perform_train_test_split(st.session_state.sampled_df, target_col_name, st.session_state.train_test_split_percentage)
-                        automl = AutoML(results_path=tmpdirname, mode="Explain")
+        if submitted:
+            with st.spinner("Generating report..."):
+                with tempfile.TemporaryDirectory() as tmpdirname:
+                    X_train, X_test, y_train, y_test = perform_train_test_split(st.session_state.sampled_df, target_col_name, st.session_state.train_test_split_percentage)
+                    automl = AutoML(results_path=tmpdirname, mode="Explain")
 
-                        with OutputRedirector() as output_string:
-                            automl.fit(X_train, y_train)
+                    with OutputRedirector() as output_string:
+                        automl.fit(X_train, y_train)
 
-                            with st.expander("Report", expanded=True):
-                                with open(f"{tmpdirname}/README.md", "r") as report_file:
-                                    st.markdown(report_file.read())
-                                    # TODO: issue number 4336
+                        with st.expander("Report", expanded=True):
+                            with open(f"{tmpdirname}/README.md", "r") as report_file:
+                                st.markdown(report_file.read())
+                                # TODO: issue number 4336
 
-                            with st.expander("Logs", expanded=False):
-                                st.text(output_string.getvalue())
+                        with st.expander("Logs", expanded=False):
+                            st.text(output_string.getvalue())
 
-                        with st.expander("Predictions for test data", expanded=False):
-                            predictions = automl.predict(X_test)
-                            st.write(predictions)
+                    with st.expander("Predictions for test data", expanded=False):
+                        predictions = automl.predict(X_test)
+                        st.write(predictions)
 
-                        # save dir as zip to session_state
-                        st.session_state.explain_zip_buffer = io.BytesIO()
-                        zip_directory_into_buffer(tmpdirname, st.session_state.explain_zip_buffer)
+                    # save dir as zip to session_state
+                    st.session_state.explain_zip_buffer = io.BytesIO()
+                    zip_directory_into_buffer(tmpdirname, st.session_state.explain_zip_buffer)
 
-        st.success("Done!")
+            st.success("Done!")
         if explain_zip_buffer_in_session_state():
-            st.download_button("Download report zip file", st.session_state.explain_zip_buffer.getvalue(), "automl_report.zip")
+            current_datetime = datetime.now()
+            formatted_datetime = current_datetime.strftime("%H_%M_%S-%d_%m_%Y")
+            st.download_button("Download report zip file", st.session_state.explain_zip_buffer.getvalue(), f"automl_report-{formatted_datetime}.zip")
         # TODO: show results
