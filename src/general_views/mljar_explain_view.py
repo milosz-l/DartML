774adc6226b0c0479debea89b58a105c1e2e3d12
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-06-01 14:04:41 +0200

    fixed predictions column name

--- src/general_views/mljar_explain_view.py
+++ src/general_views/mljar_explain_view.py
@@ -101,8 +101,8 @@ def train_mljar_explain(target_col_name, tmpdirname, problem_type, eval_metric, 
         # save predictions
         if X_test is not None:
             predictions = automl.predict(X_test)
-            predictions_df = pd.DataFrame(predictions)
-            predictions_df.to_csv(f"{tmpdirname}/{TEST_PREDICTIONS_FILENAME}", index=False, columns=["predictions"])
+            predictions_df = pd.DataFrame(predictions, columns=["predictions"])
+            predictions_df.to_csv(f"{tmpdirname}/{TEST_PREDICTIONS_FILENAME}", index=False)
 
         # save redirected logs to session_state
         st.session_state.redirected_training_output = output_string.getvalue()
@@ -159,3 +159,7 @@ def show_mljar_assess():
                 f"automl_report-{formatted_datetime}.zip",
                 help="Download data from last experiment (whole report and all trained models)",
             )
+
+            # st.divider()
+            # # show test data results
+            # with st.expander("Test data")
