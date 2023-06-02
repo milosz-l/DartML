7fc81b23aafff3f345e5a7ed9832874113b2ae66
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-06-02 22:44:25 +0200

    added altair equal df test

--- src/explore/plots_view.py
+++ src/explore/plots_view.py
@@ -110,17 +110,15 @@ def altair_interactive_corr_heatmap(df, data_2dbinned):
     ).resolve_scale(color="independent")
 
 
-def generate_interactive_altair_corr_heatmap():
+def generate_interactive_altair_corr_heatmap(df):
     def get_non_numeric_columns_names(df):
         non_numeric_cols = [col for col in df.columns if not pd.api.types.is_numeric_dtype(df[col])]
         return non_numeric_cols
 
-    # sampled_df_copy = st.session_state.sampled_df.copy()
-
-    df = st.session_state.sampled_df
     data_2dbinned = get_binned(df, get_non_numeric_columns_names(df))
     return altair_interactive_corr_heatmap(df, data_2dbinned)
 
 
 def show_altair_plots():
-    st.altair_chart(generate_interactive_altair_corr_heatmap())
+    df = st.session_state.sampled_df
+    st.altair_chart(generate_interactive_altair_corr_heatmap(df))
---
+++ tests/altair_test.py
@@ -0,0 +1,18 @@
+import pytest
+from src.explore.plots_view import generate_interactive_altair_corr_heatmap
+import pandas as pd
+
+
+def test_df_after_generating_altair_plot_is_the_same():
+    """
+    Test if DataFrame is the same after generating altair plot
+    """
+    # create df with both numerical and categorical columns
+    starting_df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": ["a", "b", "c"]})
+    expected_df = starting_df.copy()
+
+    # generate altair plot
+    generate_interactive_altair_corr_heatmap(starting_df)
+
+    # check if df is the same
+    assert starting_df.equals(expected_df)
